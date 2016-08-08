2a9791bb71c33d64a1131ca131826960c2ee3769
ixqs <shen_x_q@sina.com>
2016-08-08 10:55:23 +0800

    init

---
+++ SHNumber0/SHNumber0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SHNumber0
+//
+//  Created by sxq on 16/8/8.
+//  Copyright © 2016年 sxq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
